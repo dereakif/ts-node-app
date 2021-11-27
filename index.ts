668b770ac731158041ef23a485151be6d35f1756
Akif <dereakif@gmail.com>
2021-11-27 23:24:40 +0300

    implement: updateTodo

--- index.ts
+++ index.ts
@@ -1,7 +1,13 @@
 import bodyParser from "body-parser";
 import express from "express";
 import cors from "cors";
-import { addTodo, deleteTodo, todos, validateTodo } from "./data/todos";
+import {
+  addTodo,
+  deleteTodo,
+  todos,
+  updateTodo,
+  validateTodo,
+} from "./data/todos";
 import { Todo, TodoInput } from "./types";
 const app = express();
 const PORT = 3000;
@@ -12,6 +18,15 @@ app.use(cors());
 
 app.get("/todos", (req, res) => {
   res.status(201).send(todos);
+});
+
+app.put("/todos", (req, res) => {
+  const { todoId } = req.body;
+  const id = updateTodo(todoId);
+  if (id) {
+    return res.status(201).send({ id });
+  }
+  res.status(400).send("Couldn't update the todo");
 });
 
 app.delete("/todos", (req, res) => {
